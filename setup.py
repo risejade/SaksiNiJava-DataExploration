c0fcb800ce5f20d0cfc73c8af0f41074e336015d
Rise Jade Benavente <risejade.benavente@cit.edu>
2024-10-04 14:32:15 +0800

    new update

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     python_requires='>=3.6',  # Adjust according to your requirements
     install_requires=[
         'matplotlib==3.7.1',
-        'numpy==1.23.0',
+        'numpy==1.24.0',
         'pandas==2.1.0',
         'scipy==1.11.2',
         'seaborn==0.12.2',
